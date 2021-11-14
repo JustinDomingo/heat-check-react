af4598eb10f38320e0d0851df3eb945ef1bcbb50
Justin <jdomingo122601@gmail.com>
2021-11-13 20:47:35 -0800

    Images

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+
+const api = {
+  key: 'b6f4a9b8b445a64693bc2cbb86bbe204',
+  base: 'api.openweathermap.org/data/2.5/'
+}
+
+function Icon(props) {
+  return <img className="icon-img" src={`http://openweathermap.org/img/wn/${props.source}@2x.png`}/>
+}
 
 function App() {
+  const [query, setQuery] = useState('')
+  const [weather, setWeather] = useState({})
+
+  function getData(e) {
+    e.preventDefault()
+    fetch(`https://${api.base}weather?q=${query}&appid=${api.key}&units=imperial`)
+        .then(res => res.json())
+        .then(data => {
+          setWeather(data)
+        })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <body>
+    <center>
+      <div className={(typeof weather.main != "undefined") ? ((weather.main.temp < 80) ? 'container cold' : 'container hot') : 'container default' }>
+          <form onSubmit={getData}>
+            <input type="text" placeholder="Search for a city..." className="input" onChange={e => setQuery(e.target.value)}></input>
+          </form>
+          {(typeof weather.main != "undefined") ? (
+            <div>
+              <div className="state-date-container">
+            <div className="city">{weather.name}, {weather.sys.country}</div>
+            <div className="date">{new Date().toLocaleString()}</div>
+          </div>
+          <div className="weather-container">
+            <div className="weather">{weather.weather[0].main}</div>
+            <div className="temperature">{Math.round(weather.main.temp)}°</div>  
+            <Icon source={weather.weather[0].icon}/>        
+          </div>
+            </div>
+          ) : ('')}
+      </div>
+    </center>
+  </body>
   );
 }
 
